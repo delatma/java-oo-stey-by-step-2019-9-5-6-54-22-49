61c31f76b3a38bfd46cc817e15f8fd006aba61a5
Mark Anthony De la Torre <mark.anthony.de.la.torre@oocl.com>
2019-09-23 14:51:22 +0800

    update practice02

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,23 @@
 package practice02;
 
 public class Person {
+    private String name;
+    private int age;
+
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    int getAge(){
+        return age;
+    }
+
+    String introduce(){
+        return "My name is " + getName() + ". I am " + getAge() +" years old.";
+    }
 }
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,30 @@
 package practice02;
 
 public class Student {
+    private String name;
+    private int age;
+    private int classNumber;
+
+    Student(String name, int age, int classNumber){
+        this.name = name;
+        this.age = age;
+        this.classNumber = classNumber;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    int getAge(){
+        return age;
+    }
+
+    int getKlass(){
+        return classNumber;
+    }
+
+    String introduce(){
+        return "I am a Student. I am at Class " + getKlass() + ".";
+    }
+
 }

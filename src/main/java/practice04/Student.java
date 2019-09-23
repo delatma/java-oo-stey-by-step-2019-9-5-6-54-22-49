41fe24bdaf81433c30c5ada2c734d9158389e1b2
Mark Anthony De la Torre <mark.anthony.de.la.torre@oocl.com>
2019-09-23 17:44:31 +0800

    update practice04

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,25 @@
 package practice04;
 
-public class Student {
+public class Student extends Person{
+    //    Then create class Student to inherit class Person. There are fields name, age, klass
+    private int klass;
+
+    Student(String name, int age, int klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    int getKlass(){
+        return klass;
+    }
+    //    and a method named introduce in class Student. Method introduce should return string like this:
+    //    >My name is Tom. I am 21 years old. I am a Student. I am at Class 2.
+    //    This text from all these three classes
+    //    >My name is Tom. I am 21 years old.
+    //    should be returned by calling method backIntroduce from class Person.
+
+    String introduce(){
+        return super.introduce() + " I am a Student. I am at Class " + klass + ".";
+    }
+
 }
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,16 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+//    Then create class Worker to inherit class Person. There are fields name, age, a method named introduce in class Workder.
+    Worker(String name, int age){
+        super(name, age);
+    }
+//    Method introduce should return string like this:
+//    >My name is Tom. I am 21 years old. I am a Worker. I have a job.
+//    This text from all these three classes
+//    >My name is Tom. I am 21 years old.
+//    should be returned by calling method backIntroduce from class Person.
+    String introduce(){
+        return super.introduce() + " I am a Worker. I have a job.";
+    }
 }

433cd6f1848169c702a12d60fa02a165578953d5
Mark Anthony De la Torre <mark.anthony.de.la.torre@oocl.com>
2019-09-25 22:45:28 +0800

    update practice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,28 @@
 package practice08;
 
 public class Klass {
+    //    Create class Klass with fields number and leader.
+    private int number;
+    private Student leader;
+
+    //    But leader doesn't get assigned in the constructor.
+    Klass(int number){
+        this.number = number;
+    }
+
+    int getNumber(){
+        return number;
+    }
+
+    Student getLeader(){
+        return leader;
+    }
+
+    String getDisplayName(){
+        return "Class " + number;
+    }
+    //    Klass has a assignLeader method which receive a Student instance to assign the leader of the Klass.
+    void assignLeader(Student leader){
+        this.leader = leader;
+    }
 }

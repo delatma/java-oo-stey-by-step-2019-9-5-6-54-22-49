ab2192c338572719e5062cee566889ec46656df9
Mark Anthony De la Torre <mark.anthony.de.la.torre@oocl.com>
2019-09-25 23:45:27 +0800

    update practice11

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,52 @@
 package practice11;
 
 public class Klass {
+    //    Create class Klass with fields number and leader.
+    private int number;
+    private Student leader;
+    private Student member;
+
+    //    But leader doesn't get assigned in the constructor.
+    Klass(int number){
+        this.number = number;
+    }
+
+    int getNumber(){
+        return number;
+    }
+
+    Student getLeader(){
+        return leader;
+    }
+
+    String getDisplayName(){
+        return "Class " + number;
+    }
+
+    //A teacher will print text below when a student becomes the leader of any classes that the teacher teach:
+    //>I am Tom. I know Jerry become Leader of Class 2.
+
+    //    Klass has a assignLeader method which receive a Student instance to assign the leader of the Klass.
+    void assignLeader(Student leader){
+        if (member != null){
+            this.leader = leader;
+            System.out.print("I am Tom. I know " + leader.getName() + " become Leader of Class " + getNumber() + ".\n");
+        }
+        else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    //Klass also has a appendMember method which receive a Student instance to add a  student to this Klass.
+    //If a student is not in the Klass, then assignLeader to the student will fail and return:
+    //>It is not one of us.
+    //and if so, the method introduce of Student should just bahave like before.
+
+    //A teacher will print text below when a student joins any classes that the teacher teach:
+    //>I am Tom. I know Jerry has joined Class 2.
+    void appendMember(Student student){
+        member = student;
+        System.out.print("I am Tom. I know " + member.getName() + " has joined Class " + getNumber() + ".\n");
+    }
+
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,64 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person{
+    //    Then create class Teacher to inherit class Person. There are fields id, name, age, classes
+    private LinkedList<Klass> classes;
+    private Klass klass;
+    private Student student;
+
+    Teacher(int id, String name, int age, LinkedList<Klass> classes){
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    Teacher(int id, String name, int age){
+        super(id, name, age);
+    }
+
+    Klass getKlass(){
+        return klass;
+    }
+
+    public LinkedList<Klass> getClasses(){
+        return classes;
+    }
+//and a method named introduce in class Teacher. Method introduce should return string like this:
+//>My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2, 3.
+//if the length of classes is zero, then it should return string like this:
+//>My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.
+
+    String introduce(){
+        if (getClasses() != null && !getClasses().isEmpty()){
+            String classes = "";
+            for (Klass klass: getClasses()){
+                classes +=Integer.toString(klass.getNumber()).concat(", ");
+            }
+            return super.introduce() + " I am a Teacher. I teach " + "Class " + classes.substring(0,classes.length()-2)+".";
+        }
+        else return super.introduce() + " I am a Teacher. I teach " + "No Class.";
+    }
+
+    //This text from all subclasses of class Person
+    //>My name is Tom. I am 21 years old.
+    //should be returned by calling method introduce from class Person.
+
+    String introduceWith(Student student){
+        if (isTeaching(student))
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+    }
+    //    Teacher also has a isTeaching method which receive a Student instance to return it's ture
+    //    or false that the teacher is teaching the student. Once the student is in any classes that
+    //    teacher is teaching, the method should return true.
+    //    And there is a corresponding method named isIn in class Student to return is the student is in the klass.
+    boolean isTeaching(Student student){
+        for (Klass klass : getClasses()){
+            if (klass.getNumber() == student.isIn())
+                return true;
+        }
+        return false;
+    }
+
 }
